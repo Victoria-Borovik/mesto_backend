c29fad701ec9446563ed251f52d480072fb67e95
Victoria Kudriashova <borovik.va111@gmail.com>
2026-02-15 13:46:26 +0300

    Feat: add mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('bd');
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(3000);
