d45c8d5ff263394cc087e84d183bf96bd06400b5
Victoria Kudriashova <borovik.va111@gmail.com>
2026-02-15 13:47:02 +0300

    Feat: add card and user models

---
+++ src/models/cards.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const cardSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [{
+    type: Schema.Types.ObjectId,
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default model('card', cardSchema);
